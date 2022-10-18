7cf4bb0f4c7efafc6767b63c813f7c76b1f86451
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-10-18 11:42:10 -0700

    Variable Question solved

---
+++ variable.js
@@ -0,0 +1,28 @@
+// Declare four variables without assigning values
+let a, b, c, d;
+
+
+// Declare four variables with assigning values
+let name = "Sajid", Age = 22, phnumber = 1234565778, gender = "Male";
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Mohammed ";
+let lastName = "Qureshi";
+let martial_status = true //true==single,false==married
+let country = "India"
+let age = 22
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let fname = "Mohammed", lname = "Qureshi", status = true, plaace = "India", dob = 22;
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to browser console. Output:
+let myAge = 22
+let yourAge = 20
+
+
+console.log(myAge, yourAge);
+
+
