2cddd728672e372009d350be14c51b5cf3b468bb
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-10-18 11:52:25 -0700

    DataTypes

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+// Declare variables and assign string, boolean, undefined and null data types
+
+let name = "Sajid", Active = true, status; idNo = null;
+
+
+
+// The JavaScript typeof operator uses to check different data types. Check the data type of each variables from question number 1.
+
+console.log(typeof Active);
+console.log(typeof status);
+console.log(typeof idNo);
+console.log(typeof name);
