164604444bf4008fe4745c2fe06acfc908a6ae8b
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-10-18 11:59:36 -0700

    String problems

---
+++ string.js
@@ -0,0 +1,10 @@
+// - Declare a variable name company and assign it to an initial value 'Coding Academy'.
+
+var company = 'Coding Academy'
+
+// - Print the string on the browser console using console.log()
+
+console.log(company);
+
+// - Print the length of the string on the browser console using console.log()
+console.log(company.length);
